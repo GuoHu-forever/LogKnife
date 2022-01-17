4cb548bfa5d46220bc54a3703f368418e86370ec
guohu <guohu@xiaomi.com>
2022-01-17 10:35:54 +0800

    add search bar

--- js/main.js
+++ js/main.js
@@ -20,10 +20,53 @@
            
         }
     });
+    var gResults;
+    var searchDom=document.getElementById("search");
+    searchDom.onkeydown=function(event){
+        var code = event.keyCode;
+        if(code ===13){ //这是键盘的enter监听事件
+        //绑定焦点，有可能不成功，需要多试试一些标签 
+           // @ts-ignore
+           var searchText=searchDom.value;
+           console.log("searchText:"+searchText);
+           searchInResults(searchText);
+         }
+    };
+
+
+    function searchInResults(searchText){
+         if(!gResults||!searchText){
+             console.log("gResults: "+gResults);
+            return;
+         }
+         var regexText=new RegExp(searchText);
+        var container=document.getElementById('container');
+        container.innerHTML="";
+        for(var i=0;i<gResults.length;i++){
+            if(regexText.test(gResults[i].text)){
+                console.log("匹配:"+gResults[i].text);
+                var p=document.createElement("div");
+                var lineNumberNodeStr="<span style='color:blue'>"+(gResults[i].lineBegin+1)+"</span>";
+                p.innerHTML=lineNumberNodeStr+space+"<span id="+i+">"+gResults[i].text+"\n"+"</span>";
+                p.style.color=gResults[i].color;
+                p.setAttribute("line",gResults[i].lineBegin.toString());
+                p.setAttribute("columnEnd",gResults[i].columnEnd.toString());
+                //p.setAttribute("id",i.toString());
+                container.appendChild(p);
+            }
+
+         
+        } 
+         document.getElementById('container').addEventListener('dblclick',function(event){
+            jump(event.target);
+        },false);
+
+    }
+
     function viewResultBySearch(results){
         var container=document.getElementById('container');
         container.innerHTML="";
-  
+        gResults=results;
         for(var i=0;i<results.length;i++){
             var p=document.createElement("div");
             var lineNumberNodeStr="<span style='color:blue'>"+(results[i].lineBegin+1)+"</span>";
--- src/searchWebViewProvider.ts
+++ src/searchWebViewProvider.ts
@@ -1,30 +1,30 @@
 import * as vscode from 'vscode';
-import { ResultItem,searchFilters} from './searchCommands';
+import { ResultItem, searchFilters } from './searchCommands';
 import { Filter } from "./utils";
 export class SearchWebViewProvider implements vscode.WebviewViewProvider {
 
 	public static readonly viewType = 'searchWebView';
 
 	private _view?: vscode.WebviewView;
-    private _results?:ResultItem[];
-	private _doc?:vscode.TextDocument;
-	private _readCache:boolean=true;
+	private _results?: ResultItem[];
+	private _doc?: vscode.TextDocument;
+	private _readCache: boolean = true;
 
 	constructor(
 		private readonly _extensionUri: vscode.Uri,
 	) { }
-    
+
 	public resolveWebviewView(
 		webviewView: vscode.WebviewView,
 		context: vscode.WebviewViewResolveContext,
 		_token: vscode.CancellationToken,
 	) {
-		
+
 		console.log("resolveWebviewView");
-		
-        
+
+
 		this._view = webviewView;
- 
+
 		webviewView.webview.options = {
 			// Allow scripts in the webview
 			enableScripts: true,
@@ -39,9 +39,9 @@ export class SearchWebViewProvider implements vscode.WebviewViewProvider {
 			switch (data.type) {
 				case 'jump':
 					{
-						
+
 						this.jump(data.value);
-                        break;
+						break;
 					}
 			}
 		});
@@ -49,12 +49,12 @@ export class SearchWebViewProvider implements vscode.WebviewViewProvider {
 		// 	vscode.commands.executeCommand("searchWebView.removeView");
 		// });
 	}
-    private _getHtmlForWebview(webview: vscode.Webview) {
-        
-     // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+	private _getHtmlForWebview(webview: vscode.Webview) {
+
+		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
 		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'js', 'main.js'));
 
-        return `<!DOCTYPE html>
+		return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -63,99 +63,133 @@ export class SearchWebViewProvider implements vscode.WebviewViewProvider {
 				
 				<title>Log Knife</title>
 				<style>
-				*{
-					font-family:-moz-fixed;
-					white-space:pre;
-					margin:4px;
-					font-size:14px;
+					*{
+						font-family:-moz-fixed;
+						white-space:pre;
+						margin:4px;
+						font-size:14px;
 
-		          } 
+					} 
+					.searchContainer {
+						position:fixed;
+						background-color: white;
+						top:0px;
+						left:0px;
+						right:0px;
+						height: 30px;
+						width: 100%;
+						margin: auto auto 0 auto;
+						 /*border: rgb(158, 16, 16) solid 2px;*/ 
+						text-align: center;
+					}
+			
+					.search {
+						position:fixed;
+						top:0px;
+						left:10px;
+						right:10px;
+						width: 90%;
+						height: 30px;
+						border-radius: 18px;
+						outline: none;
+						border: 1px solid #ccc;
+						padding-left: 40px;
+						margin: auto auto 0 auto;
+					}
+			
+				
 				  </style>       
 			</head>
-			<body id="container">
-				<script  src="${scriptUri}"></script>
+			<body>
+			      <div class="searchContainer">
+		             	<input type="text"  id="search" class="search" placeholder="搜索"/>
+		          </div>
+
+				  <div  id="container" class="container"> 
+			
+				 </div>
+				 <script  src="${scriptUri}"></script>
 			</body>
 			</html>`;
 	}
-    public webViewSearchFilters(doc: vscode.TextDocument | undefined,filters:Filter[]){
+	public webViewSearchFilters(doc: vscode.TextDocument | undefined, filters: Filter[]) {
 		console.log("webViewSearchFilters");
-		console.log("fiters:  "+filters);
-		this._doc=doc;
-		
-        this._results=searchFilters(doc,filters);
-		console.log("results: "+this._results);
-		
-        if(typeof this._results===undefined){
+		console.log("fiters:  " + filters);
+		this._doc = doc;
+
+		this._results = searchFilters(doc, filters);
+		console.log("results: " + this._results);
+
+		if (typeof this._results === undefined) {
 			return;
 		}
-	
 
-        this.viewResult();
-	
 
-    }
-    
-    public viewResult() {
-		
-		 vscode.commands.executeCommand("workbench.view.extension.searchWebViewContainer").then(()=>{
-			 console.log("Webview up！");
-			 
+		this.viewResult();
+
+
+	}
+
+	public viewResult() {
+
+		vscode.commands.executeCommand("workbench.view.extension.searchWebViewContainer").then(() => {
+			console.log("Webview up！");
+
 			if (this._view) {
 				this._view.show(false);
 				console.log("viewResult");
-				this._view.webview.postMessage({ type: 'viewResult',value:this._results});
-		
-				for(var i=0;i<this._results!.length;i++){
-					var r=this._results![i];
-					var docorationType=vscode.window.createTextEditorDecorationType({
+				this._view.webview.postMessage({ type: 'viewResult', value: this._results });
+
+				for (var i = 0; i < this._results!.length; i++) {
+					var r = this._results![i];
+					var docorationType = vscode.window.createTextEditorDecorationType({
 						color: `${r.color}`,
-						fontWeight:"bold"
-					 //	gutterIconPath:`\$(chevron-right)`
+						fontWeight: "bold"
+						//	gutterIconPath:`\$(chevron-right)`
 					});
 					let editor = vscode.window.activeTextEditor;
 					editor!.setDecorations(docorationType, [new vscode.Range(r.lineBegin, r.columnBegin, r.lineEnd, r.columnEnd)]);
-						
-				}		
-		
+
+				}
+
 			}
-		 }
+		}
 
 		);
-	
+
 	}
 
-    public jump(value:any){
-     
-	   
-        let line:number=value.line;
-        let columnEnd:number=value.columnEnd;
-		console.log("line "+line.toString());
-		console.log("columnEn "+columnEnd.toString());
-		
-		
-    // Make sure document is showing
-	    if(!this._doc){
+	public jump(value: any) {
+
+
+		let line: number = value.line;
+		let columnEnd: number = value.columnEnd;
+		console.log("line " + line.toString());
+		console.log("columnEn " + columnEnd.toString());
+
+
+		// Make sure document is showing
+		if (!this._doc) {
 			return;
 		}
-		
-        vscode.window.showTextDocument(this._doc).then(
-            () => {},
-            () => {}
-        );
-        if (typeof vscode.window.activeTextEditor !== "undefined") {
+
+		vscode.window.showTextDocument(this._doc).then(
+			() => { },
+			() => { }
+		);
+		if (typeof vscode.window.activeTextEditor !== "undefined") {
 			console.log(vscode.window.activeTextEditor);
-            // Make the result visible
-            vscode.window.activeTextEditor.revealRange(new vscode.Range(line, 0, line, columnEnd));
-    
-            // Select the result
-            vscode.window.activeTextEditor.selection = new vscode.Selection(line, 0, line, columnEnd);
-        }
-	
-    }
-        
+			// Make the result visible
+			vscode.window.activeTextEditor.revealRange(new vscode.Range(line, 0, line, columnEnd));
+
+			// Select the result
+			vscode.window.activeTextEditor.selection = new vscode.Selection(line, 0, line, columnEnd);
+		}
+
+	}
+
 }
 
 
 
 
- 
